ac594e29c18644294ae91c35f31cad2002431207
lburk87 <67616760+lburk87@users.noreply.github.com>
2020-08-24 20:19:01 -0500

    corrected spelling

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -26,9 +26,10 @@ public class Menu {
     public static void main(String[] args) {
         MenuItem fries = new MenuItem("fries", 2.99, "fried potatoes", "appetizer");
         MenuItem mozzSticks = new MenuItem("mozzarella sticks", 6.99, "fried cheese", "appetizer");
-        MenuItem chocMeringues = new MenuItem("choclate meringues", 4.39, "light, sweet, delish", "dessert");
+        MenuItem chocMeringues = new MenuItem("chocolate meringues", 4.39, "light, chocolatey, delish", "dessert");
         addMenuItem(fries);
         addMenuItem(mozzSticks);
+        addMenuItem(chocMeringues);
         for (int i = 0; i < menuItems.size(); i++) {
             System.out.println(menuItems.get(i).getName().toUpperCase() + " - " + menuItems.get(i).getDescription() + " - $" + menuItems.get(i).getPrice());
         }
